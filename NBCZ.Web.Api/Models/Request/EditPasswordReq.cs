c7e6dd9c8e362e6c1e1af327e8f0a223ed74d576
chi8708 <983143462@qq.com>
2021-07-08 11:12:46 +0800

    Common 添加ExcelHelper 、LimitedConcurrencyLevelTaskScheduler、ReflexUtil及修改查询攻击

---
+++ NBCZ.Web.Api/Models/Request/EditPasswordReq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NBCZ.Web.Api.Request
+{
+    public class EditPasswordReq
+    {
+        /// <summary>
+        /// 原密码
+        /// </summary>
+        public string OldPassword { get; set; }
+
+        /// <summary>
+        /// 新密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
